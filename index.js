11828a2a952f888dad5cea156374ee5140a5dccd
giannayan <giannayan1@gmail.com>
2018-07-12 23:01:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+function printBadges(names){
+  for (var count = 0; count <names.length; count++){
+    console.log("Welcome " + names[count] +"! You are employee #" + (count+1) +".");
+  }
+  return names
+}
+
+function tailsNeverFails() {
+return Math.random() >= 0.5;
+for (var count = 0; count <2 count++){
+}
+while (()) {
+  console.log('You got '+ (count)+' tails in a row!');
+}
+
+
+
+ 
+
+
+
